24d75a4e48ff63fee3223b5caf68506929abc69b
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-07 12:34:40 +0900

    remove the culprit of build failure

--- server/api/expenses/index.get.ts
+++ server/api/expenses/index.get.ts
@@ -1,10 +1,3 @@
-import { Prisma } from "@prisma/client";
-export type ExpenseWithPayer = Prisma.ExpenseGetPayload<{
-  include: {
-    payer: true;
-  };
-}>;
-
 export default defineEventHandler(async (event) => {
   const expenses = await prisma.expense.findMany({
     include: {
